2a15a3f2be41a831b3f556df844c5f732256be18
Alex19998282 <36903262+Alex19998282@users.noreply.github.com>
2018-02-28 03:59:58 +0000

    Added actual program

---
+++ MemeBotSetup.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MemeBotCode
+{
+    class MemeBotSetup
+    {
+        private IServiceProvider services;
+
+        public static void Main(string[] args)
+            => new MemeBotSetup().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            CommandService commands = new CommandService();
+
+            string token = "token here"; //This should be in a separate file for the final build on GitHub
+
+            services = new ServiceCollection().BuildServiceProvider();
+
+            client.Log += LogHandler;
+            client.MessageReceived += (messageParameter) => MessageReceived(messageParameter, commands, client);
+
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task LogHandler(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage messageParameter, CommandService commands, DiscordSocketClient client)
+        {
+            SocketUserMessage message = messageParameter as SocketUserMessage;
+            int cmdPos = 0;
+
+            if (message != null && message.HasCharPrefix('!', ref cmdPos))
+            {
+                CommandContext context = new CommandContext(client, message);
+                var result = await commands.ExecuteAsync(context, cmdPos, services);
+
+                if(!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
