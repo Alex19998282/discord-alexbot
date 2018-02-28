3a8fb1993515be583eb3b8243bf6b54406e24de0
Alex19998282 <36903262+Alex19998282@users.noreply.github.com>
2018-02-28 19:51:32 +0000

    Small changes to the setup file

--- MemeBotSetup.cs
+++ MemeBotSetup.cs
@@ -11,6 +11,7 @@ namespace MemeBotCode
     class MemeBotSetup
     {
         private IServiceProvider services;
+        private CommandService commands;
 
         public static void Main(string[] args)
             => new MemeBotSetup().MainAsync().GetAwaiter().GetResult();
@@ -18,9 +19,9 @@ namespace MemeBotCode
         public async Task MainAsync()
         {
             DiscordSocketClient client = new DiscordSocketClient();
-            CommandService commands = new CommandService();
+            commands = new CommandService();
 
-            string token = "token here"; //This should be in a separate file for the final build on GitHub
+            string token = "NDE4MjUyNDEzMzU1Njg3OTM3.DXiXlA.-1MsV8QDd5K19oOLpKp-CfXqyVU"; //This should be in a separate file for the final build on GitHub
 
             services = new ServiceCollection().BuildServiceProvider();
 
@@ -49,6 +50,7 @@ namespace MemeBotCode
             if (message != null && message.HasCharPrefix('!', ref cmdPos))
             {
                 CommandContext context = new CommandContext(client, message);
+                IntroModule.setCommandService(commands);
                 var result = await commands.ExecuteAsync(context, cmdPos, services);
 
                 if(!result.IsSuccess)
