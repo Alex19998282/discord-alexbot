64efc47e649453e84d7ae5fc8a5860ab716bae1e
bakerd13 <daren.baker06@gmail.com>
2018-03-01 14:52:47 +0000

    added new files

---
+++ MemeBot/IBot.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Commands;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MemeBot
+{
+    public interface IBot
+    {
+        Task Run();
+
+        IEnumerable<CommandInfo> GetCommands();
+    }
+}
---
+++ MemeBot/Modules/MathsBot.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemeBot.Modules
+{
+    [Group("maths")]
+    public class MathsBot : ModuleBase
+    {
+        [Command("multiply"), Summary("multiply")]
+        public async Task Multiply(int a, int b)
+        {
+            await ReplyAsync("The result is: " + a * b);
+        }
+    }
+}
